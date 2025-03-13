19ef4a56396078479b5549ef81f318d82714b65e
Ollie1o1 <102271756+Ollie1o1@users.noreply.github.com>
2025-03-13 19:11:32 -0400

    Add files via upload

---
+++ Assignment 3/src/playSongGivenName.c
@@ -0,0 +1,21 @@
+#include "../include/givenA3.h"
+#include <stdio.h>
+#include <string.h>
+
+int playSongGivenName(A3Song *headLL, char givenSongName[MAX_LENGTH]) {
+    A3Song *current = headLL;
+    while (current != NULL) {
+        if (strcmp(current->songName, givenSongName) == 0) {
+            printf("Song ID: %d\n", current->songId);
+            printf("Song Name: %s\n", current->songName);
+            printf("Notes: ");
+            for (int i = 0; i < 21; i++) {
+                printf("%s.", current->songNotes[i]);
+            }
+            printf("\n");
+            return 1;
+        }
+        current = current->nextSong;
+    }
+    return -1;
+}
